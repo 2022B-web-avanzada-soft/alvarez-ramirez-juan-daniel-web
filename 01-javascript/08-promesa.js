486f59291c7fe7ff2f977e51394085c96840c353
Juan Daniel Alvarez <55715973+JuanDanielAlvarez@users.noreply.github.com>
2022-12-15 22:15:02 +0000

    [ADD] inquirer node

---
+++ 01-javascript/08-promesa.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf-8', (error, contenidoArchivo) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(contenidoArchivo);
+            }
+        });
+    });
+}
+
+
+async function asyncAwaitUno(path) {
+    try {
+        const respuestaContenidoArchivoOriginal = await readFile(path)
+        console.log(respuestaContenidoArchivoOriginal);
+    }
+    catch (error) {
+    }
+}
+
+const asyncAwaitDos = async function () {
+    console.log("hola")
+}
+
+const asyncAwaitTres = async () => {
+}
+
+asyncAwaitUno('./01-variables.js')
+asyncAwaitDos()
